cd6c18bd40cacc6c949a4562ba37ad2afb1b7d99
rafaelfiap <rm557837@fiap.com.br>
2025-04-22 21:33:29 -0300

    Primeiro commit

---
+++ src/test/java/br/com/fiap/mercadomedieval/MercadoMedievalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mercadomedieval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoMedievalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
